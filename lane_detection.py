5d21765a773045540cb68870b75c0aa07b15048d
Joe Black <156119500+jrb00013@users.noreply.github.com>
2025-03-11 09:33:35 -0400

    Update lane_detection.py

--- lane_detection.py
+++ lane_detection.py
@@ -1,5 +1,13 @@
 import cv2
+import rclpy
+from rclpy.node import Node
 import numpy as np
+import math
+import time
+# TODO: include needed ROS msg type headers and libraries
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
+from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
 
 def detect_lanes(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
